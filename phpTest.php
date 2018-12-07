12ad527beb805d7a2e36e0d77543246053ac8be0
Amisha Jain <amishaj001@gmail.com>
2018-12-07 18:05:04 +0000

    Add new files and directory

--- phpTest.php
+++ phpTest.php
@@ -7,15 +7,5 @@
     $db = "sherlock_db";                                  //Your database name you want to connect to
     $port = 3306;                                //The port #. It is always 3306
     
-    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
-
-//And now to perform a simple query to make sure it's working
-    $query = "SELECT * FROM SherlockCharacters";
-    $result = mysqli_query($connection, $query);
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "The character's name is " . $row['CharacterName'];
-        echo "<br>";
-        echo " and the actor's name is " . $row['ActorName'];
-        echo "<br>";
-    }
+    ?>
+    
